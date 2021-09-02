2ac45c428ef2b2747a3275bcca59ee39c1233c98
Georges <gkgobith@gmail.com>
2021-09-02 19:38:06 +0200

    navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import IconifyIcon from "@iconify/svelte";
   import runIcon from "@iconify-icons/bx/bx-run";
+  import gridIcon from "@iconify-icons/bx/bx-grid-alt";
+  import chevronDownIcon from "@iconify-icons/bx/bx-chevron-down";
 
   import Router from "svelte-spa-router";
   import { wrap } from "svelte-spa-router/wrap";
@@ -89,11 +91,36 @@
   </li>
 </ul> -->
 
-<div class="sidebar">
+<div class="sidebar close">
   <div class="logo-details">
     <i><IconifyIcon icon={runIcon} /></i>
-    <span class="logo_name">ACWaterland</span>
+    <span class="logo_name">AC Waterland</span>
   </div>
+  <ul class="nav-links">
+    <li>
+      <div class="icon-link">
+        <a href="#/schedules">
+          <i><IconifyIcon icon={gridIcon} /></i>
+          <span class="link_name">Rooster</span>
+        </a>
+        <i><IconifyIcon icon={chevronDownIcon} /></i>
+      </div>
+      <ul class="sub-menu">
+        <li>
+          <a class="link_name" href="#" >Rooster</a>
+        </li>
+        <li>
+          <a href="#">Oud Papier</a>
+        </li>
+        <li>
+          <a href="#">Schoonmaak</a>
+        </li>
+        <li>
+          <a href="#">Bar Dienst</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
 </div>
 
 <Router {routes} />
@@ -118,14 +145,122 @@
   {/if}
 {/if} -->
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap");
+
   :global(* body) {
     margin: 0px;
     padding: 0px;
     box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
+    font-family: "Poppins", sans-serif;
   }
 
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+  .sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 260px;
+    background: #11101d;
+  }
 
-  
+  .sidebar .logo-details {
+    height: 60px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .sidebar .logo-details i {
+    font-size: 30px;
+    color: #fff;
+    height: 50px;
+    min-width: 78px;
+    text-align: center;
+    line-height: 50px;
+  }
+
+  .sidebar .logo-details .logo_name {
+    font-size: 22px;
+    color: #fff;
+    font-weight: 600;
+  }
+
+  .sidebar .nav-links {
+    height: 100%;
+    padding: 0px;
+    padding-top: 30px;
+  }
+
+  .sidebar .nav-links li {
+    position: relative;
+    list-style: none;
+    transition: all 0.4s ease;
+  }
+
+  .sidebar .nav-links li:hover {
+    background: #1d1b31;
+  }
+
+  .sidebar .nav-links li .icon-link {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .sidebar .nav-links li i {
+    display: inline-block;
+    height: 50px;
+    min-width: 78px;
+    text-align: center;
+    line-height: 50px;
+    color: #fff;
+    font-size: 20px;
+  }
+
+  .sidebar .nav-links li a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+  }
+
+  .sidebar .nav-links li a .link_name {
+    font-size: 18px;
+    font-weight: 400;
+    color: #fff;
+  }
+
+  .sidebar .nav-links li .sub-menu {
+    padding: 6px 6px 14px 80px;
+    margin-top: -10px;
+    background: #1d1b31;
+    /* display: none; */
+  }
+
+  .sidebar .nav-links li .sub-menu a {
+    color: #fff;
+    font-size: 15px;
+    padding: 5px 0;
+    white-space: nowrap;
+    opacity: 0.6;
+    transition: all 0.3s ease;
+  }
+
+  .sidebar .nav-links li .sub-menu a:hover {
+    opacity: 1;
+  }
+
+  .sidebar.close .nav-links li .sub-menu {
+    position: absolute;
+    left: 100%;
+    top: 0;
+    margin-top: 0;
+    padding: 10px 20px;
+    border-radius: 0 6px 6px 0;
+  }
+
+  .sidebar.close .nav-links li .sub-menu .link_name {
+    font-size: 18px;
+    opacity: 1;
+
+  }
+
 </style>
